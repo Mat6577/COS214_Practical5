a1cf1d29563f7467376d636e510d884b56e35d22
Matome <Matmohohoma68@gmail.com>
2026-09-18 11:18:36 +0200

    Completed the implementation of the adapter pattern. This is version 1, will update after resolving questions

--- CampusComponent.h
+++ CampusComponent.h
@@ -1,6 +1,13 @@
 #ifndef CAMPUSCOMPONENT_H
 #define CAMPUSCOMPONENT_H
 
+/**
+ * @class CampusComponent
+ * @brief Utility class
+ * 
+ * @details It is used as an instance variable for the SecureAre command. It can be locked and unlocked
+ * @date 18/09/2026
+ */
 class CampusComponent {
 public:
     virtual ~CampusComponent() = default;
