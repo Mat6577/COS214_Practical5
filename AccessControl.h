b55aea996c95b05fdb90574ed92e567391dfbff9
Matome <Matmohohoma68@gmail.com>
2026-09-18 15:24:36 +0200

    Minor fixes

--- AccessControl.h
+++ AccessControl.h
@@ -11,9 +11,6 @@
 class AccessControl : public ResponseComponent {
 private:
 public:
-  AccessControl() = default;
-  virtual ~AccessControl() = default;
-
   // New access control methods
   virtual void lockArea(CampusComponent *area) = 0;
   virtual void unlockArea(CampusComponent *area) = 0;
